0e89794549ece1c8f5f5eeba510ff4db3d13b649
webstoryboy <webstupids@gmail.com>
2022-06-13 21:52:59 +0900

    2022.06.13

--- webstoryboy_portfolio/port08/assets/js/lineBanner.js
+++
@@ -1,6 +0,0 @@
-/* import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll();
-const target = document.querySelector('#linewrap');
-
-scroll.scrollTo(target); */
--- webstoryboy_portfolio/port10/assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// ---------- section3 website ------------
-// $(".screen").mouseenter(function(){
-//     $(this).css("animation","site 11s ease infinite");
-// }).mouseleave(function(){
-//     $(this).css("animation-play-state","paused");
-// })
-// 끝 ---------- section3 website ------------
